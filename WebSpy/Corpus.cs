8ade1c169415ce549e04accb6e2d168bfa172c34
kooldeji <dejiakande33@gmail.com>
2017-08-07 17:30:34 +0100

    Included my missing files(Corpus and indexer)

---
+++ WebSpy/Corpus.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSpy
+{
+    class Corpus
+    {
+        internal static Dictionary<String, Dictionary<String, int>> GetTermFrequencies()
+        {
+            var a = new Dictionary<String, Dictionary<String, int>>();
+            a["the"] = new Dictionary<string, int>();
+            a["the"]["1"] = 1;
+            a["the"]["2"] = 1;
+            a["game"] = new Dictionary<string, int>();
+            a["game"]["1"] = 2;
+            a["of"] = new Dictionary<string, int>();
+            a["of"]["1"] = 2;
+            a["life"] = new Dictionary<string, int>();
+            a["life"]["1"] = 1;
+            a["life"]["2"] = 1;
+            a["life"]["4"] = 2;
+            a["is"] = new Dictionary<string, int>();
+            a["is"]["1"] = 1;
+            a["is"]["2"] = 1;
+            a["a"] = new Dictionary<string, int>();
+            a["a"]["1"] = 1;
+            a["everlasting"] = new Dictionary<string, int>();
+            a["everlasting"]["1"] = 1;
+            a["learning"] = new Dictionary<string, int>();
+            a["learning"]["1"] = 1;
+            a["learning"]["3"] = 1;
+            a["learning"]["4"] = 2;
+            a["unexamined"] = new Dictionary<string, int>();
+            a["unexamined"]["2"] = 1;
+            a["not"] = new Dictionary<string, int>();
+            a["not"]["2"] = 1;
+            a["worth"] = new Dictionary<string, int>();
+            a["worth"]["2"] = 1;
+            a["living"] = new Dictionary<string, int>();
+            a["living"]["2"] = 1;
+            a["never"] = new Dictionary<string, int>();
+            a["never"]["3"] = 1;
+            a["stop"] = new Dictionary<string, int>();
+            a["stop"]["3"] = 1;
+            return a;
+        }
+
+        internal static HashSet<String> getDocuments()
+        {
+            return new HashSet<String>(new[] { "1", "2", "3", "4" });
+        }
+
+        internal static Dictionary<string, int> getDocuments(String term)
+        {
+            return GetTermFrequencies()[term];
+        }
+
+        internal static int getDocumentLength(string key)
+        {
+            switch (key){
+                case "1":
+                    return 10;
+                case "2":
+                    return 7;
+                case "3":
+                    return 3;
+                case "4":
+                    return 2;
+                default:
+                    return 0;
+
+            }
+        }
+
+        internal static int getNoDocuments()
+        {
+            return 3;
+        }
+    }
+}
---
+++ WebSpy/Ranker.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSpy
+{
+    public class Ranker
+    {
+        /// <summary>
+        /// The documents that have any of the query terms
+        /// </summary>
+        private HashSet<String> documents;
+        /// <summary>
+        /// An inverted index of terms mapped to a mapping of each document mapped to 
+        /// the term's TF_IDF rank.
+        /// </summary>
+        private Dictionary<String, Dictionary<String, double>> documentRank;
+        /// <summary>
+        /// The query document containing each term mapped it's TF_IDF rank.
+        /// </summary>
+        private Dictionary<String, double> query;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ranker"/> class.
+        /// </summary>
+        /// <param name="query">A mapping of each query term to the frequency in the query document.</param>
+        public Ranker( Dictionary<String, int> query)
+        {
+            if (query == null) throw new ArgumentNullException();
+            this.query = new Dictionary<string, double>();
+            documentRank = new Dictionary<string, Dictionary<string, double>>();
+            documents = new HashSet<string>();
+            CompTF_IDF(query);
+        }
+
+        public List<KeyValuePair<String, double>> Rank()
+        {
+            //A list of the relevant documents in other of relevance
+            var rank = new List<KeyValuePair<String, double>>();
+
+            //Iterates through the documents and inserts it into the right index according to 
+            //descending cosine similarity.
+            foreach (String id in documents)
+            {
+                var inserted = false;
+                var value = CosineSimilarity(id);
+                for (int i=0; i<rank.Count; i++)
+                {
+                    if (value >= rank[i].Value)
+                    {
+                        rank.Insert(i, new KeyValuePair<string, double>(id, value));
+                        inserted = true;
+                        break;
+                    }
+                }
+                if (!inserted) rank.Add(new KeyValuePair<string, double>(id, value));
+            }
+            return rank;
+        }
+        //Computes the TF-IDF rank of both the query and the documents relevant 
+        //to the document
+        private void CompTF_IDF(Dictionary<string, int> query)
+        {
+            //Compute the size of the query to compute each query's IDF
+            var querySize = 0;
+            foreach (var term in query)
+            {
+                querySize += term.Value;
+            }
+
+            foreach (KeyValuePair<String, int> term in query)
+            {
+                IDictionary<String, int> documents = Corpus.getDocuments(term.Key);
+                this.documents.UnionWith(documents.Keys);
+                var nDocuments = new Dictionary<String, double>();
+                var IDF = 1+Math.Log(1.0 * Corpus.getNoDocuments() / documents.Keys.Count);
+                this.query[term.Key] = 1.0*term.Value / querySize *IDF;
+                foreach (var item in documents)
+                {
+                    var tF = 1.0* item.Value / Corpus.getDocumentLength(item.Key) ;
+                    nDocuments[item.Key] = tF * IDF;
+                }
+                documentRank[term.Key] = nDocuments;
+                ;
+            }
+        }
+
+        //Computes the cosine similarity between the query and the document parameter
+        private double CosineSimilarity(String id)
+        {
+            double dotProduct = 0;
+            double queryMod = 0;
+            double documentMod = 0;
+            foreach (var term in query)
+            {
+                queryMod += Math.Pow(term.Value,2);
+                if (documentRank[term.Key].ContainsKey(id))
+                {
+                    documentMod += Math.Pow(documentRank[term.Key][id], 2);
+                    dotProduct += term.Value * documentRank[term.Key][id];
+                }
+            }
+            queryMod = Math.Sqrt(queryMod);
+            documentMod = Math.Sqrt(documentMod);
+            return dotProduct / (documentMod * queryMod);
+
+        }
+        public static Dictionary<TKey, TValue> NestedCopy<TKey, TValue>(
+            Dictionary<TKey, TValue> nestedDict)
+        {
+            var retDict = new Dictionary<TKey, TValue>();
+            foreach (var dict in nestedDict)
+            {
+                if (typeof(TValue).IsGenericType && typeof(TValue).GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                {
+                    retDict[dict.Key] = (TValue)NestedCopy((dynamic)dict.Value);
+                }
+                else
+                {
+                    retDict[dict.Key] = dict.Value;
+                }
+            }
+            return retDict;
+        }
+    }
+}
