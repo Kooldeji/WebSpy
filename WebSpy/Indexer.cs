05a89e3903c3c084b530650a0ec58acc63ef0420
@OluwaseunCode <oluwaseunalamu@gmail.com>
2017-10-03 17:01:48 +0100

    Indexer and Tokenizer updated

--- WebSpy/Indexer.cs
+++ WebSpy/Indexer.cs
@@ -13,80 +13,39 @@ namespace WebSpy
     {
         private Corpus _corpus;
         HashSet<string> stopWords;
+        private Tokenizer _tokenizer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Indexer"/> class.
         /// </summary>
         /// <param name="corpus">The corpus. Instantiates the corpus</param>
-        public Indexer (Corpus corpus)
+        public Indexer (Corpus corpus, Tokenizer tokenizer)
         {
             _corpus = corpus;
-
-            //Load stop words into array
-            var reader = new StreamReader("StopWords.txt");
-            stopWords = new HashSet<string>();
-            while (!reader.EndOfStream)
-            {
-                stopWords.Add(reader.ReadLine());
-            }
+            _tokenizer = tokenizer;
+            ////Load stop words into array
+            //var reader = new StreamReader("StopWords.txt");
+            //stopWords = new HashSet<string>();
+            //while (!reader.EndOfStream)
+            //{
+            //    stopWords.Add(reader.ReadLine());
+            //}
         }
         /// <summary>
         /// Indexes the file passed to it
         /// </summary>
         /// <param name="path">The path of a file.</param>
         /// <returns>Returns a list</returns>
-        public Tuple<int, List<ITermDocument>> simulateIndexer(String path)
+        public Tuple<int, List<ITermDocument>> Index(String path)
                     
         {
-            
-
-            var stemmer = new Stemmer();  //Create an instance of the Stemmer class
-            var termDict = new Dictionary<string, ITermDocument>();
-            var docDict = new Dictionary<string, IDocumentReference>();
             var textExtractor = new TextExtractor();
 
             var fileData = textExtractor.Extract(Path.Combine(_corpus.GetRepository().Result, path)); //Extract the contents of the file in the specified file path
-            var list = Regex.Split(Regex.Replace(           //Use Regex expressions to include only a selected portion of the data
-                                         fileData.Text,
-                                        "[^a-zA-Z0-9']+",
-                                        " "
-                                        ).Trim(), "\\s+");
-            
-            for (int i = 0; i < list.Length; i++)  //Loop through the list
-            {
-                var word = list[i];
 
-                //Remove stopwords
-                if (stopWords.Contains(list[i]))
-                {
-                    continue;
-                }
-                
-                var rootWord = stemmer.StemWord(word.ToLower()); 
-                ITermDocument term;
-                if (termDict.Keys.Contains(rootWord))       //Check if the word has already been added to the dictionary
-                {
-                    term = termDict[rootWord];
-                }
-                else                                        //Add words to the dictionary
-                {
-                    term = new TermDocument(rootWord);
-                    termDict.Add(rootWord, term);
-                }
-                IDocumentReference docref;
-                if (docDict.Keys.Contains(word))            //Check if document reference contains word
-                {
-                    docref = docDict[word];
-                }
-                else
-                {
-                    docref = new DocumentReference("", word);
-                    docDict.Add(word, docref);
-                    term.addDoc(docref);
-                }
-                docref.addPos(i);
-            }
-            return Tuple.Create(list.Length, termDict.Values.ToList());  
+            var text = path + fileData.Text;
+
+            return _tokenizer.Tokenize(text);  
         }
     }
 }
--- WebSpy/Tokenizer.cs
+++ WebSpy/Tokenizer.cs
@@ -1,39 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WebSpy
 {
   
     public class Tokenizer
     {
+        private Stemmer _stemmer;
+        private HashSet<string> _stopWords;
+
+        public Tokenizer(HashSet<string> stopWords)
+        {
+            _stopWords = stopWords;
+            _stemmer = new Stemmer();  //Create an instance of the Stemmer class
+
+        }
         /// <summary>
         /// Generates the token.
         /// </summary>
-        /// <param name="s">This is the string which is passed into the method.</param>
-        public void generateToken(string s)
+        /// <param name="text">This is the string which is passed into the method.</param>
+        public Tuple<int, List<ITermDocument>> Tokenize(string text)
         {
-            var newString = s.ToLower();  //Convert all characters i the string to lower case
-            string[] words = newString.Split(' '); //Add all words in the string to  array 
+            var termDict = new Dictionary<string, ITermDocument>();
+            var docDict = new Dictionary<string, IDocumentReference>();
+            //Adding Term's Path for indexing.
+            var list = new List<string>(Regex.Split(Regex.Replace(
+                                         text,
+                                        "[^a-zA-Z0-9']", //Use Regex expressions to include only a selected portion of the data
+                                        " "
+                                        ).Trim(), "\\s+"));
 
-            Dictionary<string, int> dict = new Dictionary<string, int>();  //Create a dictionary to save the number of occurece of every word in the array
-            for (int i = 0; i < words.Length - 1; i++)
+            //Document Length
+            var length = list.Count;
+
+
+            for (int i = 0; i < list.Count; i++)  //Loop through the list
             {
-                if (dict.ContainsKey(words[i]))
+                var word = list[i];
+
+                //Remove stopwords
+                if (_stopWords.Contains(list[i]))
                 {
-                    dict[words[i]] += 1;
+                    length -= 1;
+                    continue;
+                }
+
+                var rootWord = _stemmer.StemWord(word.ToLower());
+                ITermDocument term;
+                if (termDict.ContainsKey(rootWord))       //Check if the word has already been added to the dictionary
+                {
+                    term = termDict[rootWord];
+                }
+                else                                        //Add words to the dictionary
+                {
+                    term = new TermDocument(rootWord);
+                    termDict.Add(rootWord, term);
+                }
+                IDocumentReference docref;
+                if (docDict.ContainsKey(word))            //Check if document reference contains word
+                {
+                    docref = docDict[word];
                 }
                 else
                 {
-                    dict.Add(words[i], 1);
+                    docref = new DocumentReference("", word);
+                    docDict.Add(word, docref);
+                    term.addDoc(docref);
                 }
+                docref.addPos(i);
             }
-
-            //Print the dictionary
-            foreach (KeyValuePair<string, int> item in dict)
-            {
-                Console.WriteLine("Key:\t {0} Value: {1}", item.Key, item.Value);
-            }
-            Console.ReadKey();
+            return Tuple.Create(length, termDict.Values.ToList());
         }
     }
 }
